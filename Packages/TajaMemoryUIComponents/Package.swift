9d3e735a275cf3c43c13db51b0069b126079f680
Andreas Günther <andreas.guenther@adidas.com>
2024-12-20 12:46:02 +0100

    Set dependencies accordingly

--- Packages/TajaMemoryUIComponents/Package.swift
+++ Packages/TajaMemoryUIComponents/Package.swift
@@ -11,11 +11,13 @@ let package = Package(
             name: "TajaMemoryUIComponents",
             targets: ["TajaMemoryUIComponents"]),
     ],
+    dependencies: [.package(path: "TajaMemory")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TajaMemoryUIComponents"),
+            name: "TajaMemoryUIComponents",
+            dependencies: [.byName(name: "TajaMemory")]),
         .testTarget(
             name: "TajaMemoryUIComponentsTests",
             dependencies: ["TajaMemoryUIComponents"]
