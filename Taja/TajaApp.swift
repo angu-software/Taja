5093a6c92c41289b8e6591c9029abc6f8b8d4a84
Andreas Günther <andreas.guenther@adidas.com>
2024-12-20 11:38:30 +0100

    Initial project structure

---
+++ Taja/TajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TajaApp.swift
+//  Taja
+//
+//  Created by Andreas Guenther on 20.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
