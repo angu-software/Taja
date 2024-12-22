1968bce04c1a847a2cebdc653d1579e878ecdf0a
Andreas Günther <andreas.guenther@adidas.com>
2024-12-22 19:12:48 +0100

    Evaluate that new game has all cards conceiled

--- Packages/TajaMemory/Tests/TajaMemoryTests/MemoryCardGameControllerTests.swift
+++ Packages/TajaMemory/Tests/TajaMemoryTests/MemoryCardGameControllerTests.swift
@@ -15,8 +15,16 @@ class MemoryCardGameController {
 
     private(set) var gameBoard: MemoryCardGameBoard
 
+    var cards: [MemoryCard] {
+        return gameBoard.cards
+    }
+
     init(gameBoard: MemoryCardGameBoard) {
         self.gameBoard = gameBoard
+    }
+
+    func startNewGame() {
+
     }
 
     func didSelectCard(_ card: MemoryCard) {
@@ -24,13 +32,46 @@ class MemoryCardGameController {
     }
 }
 
-@Suite(.tags(.acceptanceTest))
+/* =======
+ * Work log
+ * =======
+ *
+ * No selection when game loop past selecting states
+ * Select concealed card -> card revealed
+ * Selecting revealed card -> does nothing
+ * Next game has different card order
+
+ * ACCs
+ * New game -> all cards are concealed
+ * Select one card -> reveal
+ * When second revealed card match first card -> resolved
+ * When second revealed card does not match first card -> conceal two cards
+ * When all cards revealed -> game ends
+ */
+
+@Suite
 struct MemoryCardGameControllerTests {
 
+    private let gameLoop: MemoryCardGameLoop
     private let controller: MemoryCardGameController
 
     init() {
+        self.gameLoop = MemoryCardGameLoop()
         self.controller = MemoryCardGameController(gameBoard: .fixture())
+    }
+
+    @Test(.tags(.acceptanceTest))
+    func should_begin_new_game_with_all_cards_concealed() async throws {
+        startNewGame()
+
+        #expect(allCardsConcealed())
+    }
+
+    @Test(.tags(.acceptanceTest))
+    func should_begin_new_game_with_all_cards_concealed() async throws {
+        startNewGame()
+
+        #expect(allCardsConcealed())
     }
 
     @Test
@@ -43,6 +84,14 @@ struct MemoryCardGameControllerTests {
     }
 
     // MARK: - Testing DSL
+
+    private func startNewGame() {
+        controller.startNewGame()
+    }
+
+    private func allCardsConcealed() -> Bool {
+        return controller.cards.allSatisfy({ $0.state == .concealed })
+    }
 
     private func anyConcealedCard() -> MemoryCard? {
         return controller.gameBoard.cards.first(where: { $0.state == .concealed })
