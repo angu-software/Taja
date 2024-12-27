585301aaab3eb5ee08ca84adc40f53ad2102f750
Andreas Günther <andreas.guenther@adidas.com>
2024-12-27 13:10:59 +0100

    Add UI test evaluation that cards are concealed again when not matching

--- Tests/TajaUITests/TajaUITests.swift
+++ Tests/TajaUITests/TajaUITests.swift
@@ -21,7 +21,7 @@ final class TajaMemoryUITests: XCTestCase {
     }
 
     func test_should_reveal_a_card_when_tapped() throws {
-        let card = try XCTUnwrap(choseConcealedCard())
+        let card = choseConcealedCard()
         let cardId = card.identifier
 
         tapCard(card)
@@ -29,7 +29,20 @@ final class TajaMemoryUITests: XCTestCase {
         XCTAssert(isCardRevealed(cardId))
     }
 
-    // TODO: should conceal not matching cards
+    func test_should_conceal_selected_cards_when_not_matching() async throws {
+        let firstCard = chooseCard("memoryCard_1_concealed_photo01")
+        let secondCard = chooseCard("memoryCard_3_concealed_photo02")
+        let firstCardId = firstCard.identifier
+        let secondCardId = secondCard.identifier
+
+        tapCard(firstCard)
+        tapCard(secondCard)
+
+        try await lookAtCards()
+
+        XCTAssert(isCardConcealed(firstCardId))
+        XCTAssert(isCardConcealed(secondCardId))
+    }
 
     private func startNewGame() {
         app.launch()
@@ -37,17 +50,31 @@ final class TajaMemoryUITests: XCTestCase {
                      timeout: 0.3)
     }
 
-    private func choseConcealedCard() -> XCUIElement? {
-        return app.images["memoryCard_1_concealed_photo01"].firstMatch
+    private func choseConcealedCard() -> XCUIElement {
+        return chooseCard("memoryCard_1_concealed_photo01")
+    }
+
+    private func chooseCard(_ id: String) -> XCUIElement {
+        return app.images[id]
     }
 
     private func tapCard(_ card: XCUIElement) {
         card.tap()
     }
 
+    private func lookAtCards() async throws {
+        try await Task.sleep(for: .seconds(1.1))
+    }
+
     private func isCardRevealed(_ cardId: String) -> Bool {
         var cardId = cardId
         cardId = cardId.replacingOccurrences(of: "_concealed", with: "_revealed")
+        return app.images[cardId].exists
+    }
+
+    private func isCardConcealed(_ cardId: String) -> Bool {
+        var cardId = cardId
+        cardId = cardId.replacingOccurrences(of: "_revealed", with: "_concealed")
         return app.images[cardId].exists
     }
 }
