57ee5c35f6cb7fd2bbfe7f212d36b1a996ff1b22
Andreas Günther <andreas.guenther@adidas.com>
2024-12-27 20:15:59 +0100

    Add new assets to PhotoAsset

--- Sources/PhotoAsset.swift
+++ Sources/PhotoAsset.swift
@@ -11,6 +11,27 @@ enum PhotoAsset: String, CaseIterable {
 
     case photo01
     case photo02
+    case photo03
+    case photo04
+    case photo05
+    case photo06
+    case photo07
+    case photo08
+    case photo09
+    case photo10
+    case photo11
+    case photo12
+    case photo13
+    case photo14
+    case photo15
+    case photo16
+    case photo17
+    case photo18
+    case photo19
+    case photo20
+    case photo21
+    case photo22
+    case photo23
 
     var photoName: String {
         return rawValue
